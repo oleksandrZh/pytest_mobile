6b4baf9d6a254270ffeb2648191ef55049def566
oleksandrZh <oleksandr.zhukov@zfort.com>
2018-12-20 10:05:10 -0500

    modigy test methods add comment

--- Tests/login_test_edited.py
+++ Tests/login_test_edited.py
@@ -8,6 +8,7 @@ class TestLoginEdited(BaseTest):
     # run test in command line pytest Tests/login_test_edited.py -v
 
     def test_login_as_valid_user(self, app):
+        """login to application"""
         user = User().set_valid_user()
         app.base_helper.skip_main_page()
         app.login_helper.login_to_app(user)
